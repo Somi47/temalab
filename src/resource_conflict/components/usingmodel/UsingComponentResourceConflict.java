f311fc34911be4ca9b3854f12d6a346965fd666d
Somi47 <extrackto@gmail.com>
2018-11-21 11:25:46 +0100

    Using model added
    Signed-off-by: Somi47 <extrackto@gmail.com>

---
+++ src/resource_conflict/components/usingmodel/UsingComponentResourceConflict.java
@@ -0,0 +1,69 @@
+package resource_conflict.components.usingmodel;
+
+
+import resource_conflict.components.model.Component;
+import resource_conflict.components.model.ModelFactory;
+import resource_conflict.components.model.ModelPackage;
+import resource_conflict.components.model.Person;
+import resource_conflict.components.model.Qualification;
+import resource_conflict.components.model.Task;
+
+public class UsingComponentResourceConflict {
+	 public static void main(String[] args) {
+	    	ModelPackage.eINSTANCE.eClass();
+	    	ModelFactory factory = ModelFactory.eINSTANCE;
+	    	
+	    	// OBJECTS:
+	    	Person personBela = factory.createPerson();
+	    	Person personGeza = factory.createPerson();
+	    	Qualification qualificationDesigner = factory.createQualification();
+	    	Qualification qualificationDeveloper = factory.createQualification();
+	    	Qualification qualificationTester = factory.createQualification();
+	    	Component componentSearchEngine = factory.createComponent();
+	    	Component componentFrontendGui = factory.createComponent();
+	    	Task taskDesignSearchEngine = factory.createTask();
+	    	Task taskDesignFrontendGui = factory.createTask();
+	    	Task taskPrototypeFrontendGui = factory.createTask();
+	    	Task taskTestFrontendGui = factory.createTask();
+	    	
+	    	// MEMBERS:
+	    	personBela.setName("Bela");
+	    	personGeza.setName("Geza");
+	    	qualificationDesigner.setName("Designer");
+	    	qualificationDeveloper.setName("Developer");
+	    	qualificationTester.setName("Tester");
+	    	componentSearchEngine.setName("Search engine");
+	    	componentFrontendGui.setName("Frontend gui");
+	    	taskDesignSearchEngine.setName("Desing search engine");
+	    	taskDesignSearchEngine.setEstimate(2);
+	    	taskDesignFrontendGui.setName("Desing frontend gui");
+	    	taskDesignFrontendGui.setEstimate(4);
+	    	taskPrototypeFrontendGui.setName("Prototype frontend gui");
+	    	taskPrototypeFrontendGui.setEstimate(7);
+	    	taskTestFrontendGui.setName("Test frontend gui");
+	    	taskTestFrontendGui.setEstimate(3);
+	    	
+	    	// RELATIONS:
+	    	personBela.getQualifications().add(qualificationDesigner);
+	    	personBela.getQualifications().add(qualificationDeveloper);
+	    	
+	    	personGeza.getQualifications().add(qualificationDeveloper);
+	    	personGeza.getQualifications().add(qualificationTester);
+	    	
+	    	componentSearchEngine.getTasks().add(taskDesignSearchEngine);
+	    	
+	    	componentFrontendGui.getTasks().add(taskDesignFrontendGui);
+	    	componentFrontendGui.getTasks().add(taskPrototypeFrontendGui);
+	    	componentFrontendGui.getTasks().add(taskTestFrontendGui);
+	    	
+	    	taskDesignSearchEngine.getQualificationsRequired().add(qualificationDesigner);
+	    	
+	    	taskDesignFrontendGui.getQualificationsRequired().add(qualificationDesigner);
+	    	
+	    	taskPrototypeFrontendGui.getQualificationsRequired().add(qualificationDeveloper);
+	    	taskPrototypeFrontendGui.getRequirementsToStart().add(taskDesignFrontendGui);
+	    	
+	    	taskTestFrontendGui.getQualificationsRequired().add(qualificationTester);
+	    	taskTestFrontendGui.getRequirementsToStart().add(taskPrototypeFrontendGui);
+	 }
+}
